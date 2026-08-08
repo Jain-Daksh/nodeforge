cf944debc7f99871383fc095edbcb1a317029a87
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-08-08 12:31:19 +0530

    added cond for project name

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('NodeForge CLI');
+const args = process.argv.slice(2);
+
+const projectName = args[0] || 'backend';
+
+if (!args[0]) {
+  console.log(`
+⚠ No project name provided.
+  Using default project name: ${projectName}
+`);
+}
+
+console.log('Project name:', projectName);
